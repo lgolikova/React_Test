53e9e36d54099f592a48ce7234987ace587c5e55
lgolikova <milagoul@gmail.com>
2022-11-11 18:13:05 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Chart } from "react-google-charts";
+import { SwatchesPicker } from 'react-color';
+
+
+export function App() {
+  return (
+
+<div><SwatchesPicker /></div>
+  );
+}
+
+export default App;
